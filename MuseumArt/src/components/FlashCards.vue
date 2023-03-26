948b01f8ad75b129b871a2cc688da5f4c5da3d09
Mount-Blanc <mountblanc100@gmail.com>
2023-03-26 13:22:27 -0500

    added flashcards component

---
+++ MuseumArt/src/components/FlashCards.vue
@@ -0,0 +1,11 @@
+<template>
+<h1>HELLO</h1>
+</template>
+
+<script>
+export default {
+    data () {
+
+    }
+}
+</script>
--- MuseumArt/src/components/Works.vue
+++ MuseumArt/src/components/Works.vue
@@ -10,6 +10,9 @@
 <button  class="button-56" @click="setIndia">India</button>   
 <button  class="button-56" @click="setAfrica">Africa</button>   
 <button  class="button-56" @click="setRussia">Russia</button>   
+
+<FlashCards></FlashCards>
+
 <base-card v-for="items in Art"   >
 {{ items.title }}
 {{ items.culture }}
@@ -20,117 +23,108 @@
 {{ items.artistDisplayName }}
 {{ items.artistDisplayBio }}
 {{ items.classification }}
+<button @click="showId(items.objectID)">ID</button>
 </base-card>
 
 <button class="button-56" @click="getCultureID">Load More</button>
 </template>
 
 <script>  
+import FlashCards from './FlashCards.vue'
 export default {
-  
     data() {
-    return {
-      Art: [],
-      Category:'',
-      count:0,
-      twelve:12
-    }
-},
-methods: {
-  setFrench() {
-    this.Category="France"
-    console.log(this.Category)
-    this.Art=[]
-    this.twelve=12
-    this.count=0
-
-    this.getCultureID()
-  },
-  setChina() {
-    this.Category="China"
-    console.log(this.Category)
-    this.Art=[]
-    this.twelve=12
-    this.count=0
-
-    this.getCultureID()
-  },
-  setRussia() {
-    this.Category="Russia"
-    console.log(this.Category)
-    this.Art=[]
-    this.twelve=12
-    this.count=0
-
-    this.getCultureID()
-  },
-
-  setMexico() {
-    this.Category="Mexico"
-    console.log(this.Category)
-    this.Art=[]
-    this.twelve=12
-    this.count=0
-
-    this.getCultureID()
-  },
-  setEurope() {
-    this.Category="Europe"
-    console.log(this.Category)
-    this.Art=[]
-    this.twelve=12
-    this.count=0
-
-    this.getCultureID()
-  },
-  setIndia() {
-    this.Category="India"
-    console.log(this.Category)
-    this.Art=[]
-    this.twelve=12
-    this.count=0
-
-
-    this.getCultureID()
-  },
-  setAfrica() {
-    this.Category="Africa"
-    console.log(this.Category)
-    this.Art=[]
-    this.twelve=12
-    this.count=0
-
-
-    this.getCultureID()
-  },
-    getCultureID() {
-      // Simple GET request using fetch
-      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?geoLocation=${this.Category}&q=flowers`)
-        .then(response => response.json())
-        .then(data => {
-
-          const objectID = data.objectIDs;
-          const startIndex = this.count;
-      const endIndex = this.count + this.twelve;
-      const idSubset = objectID.slice(startIndex, endIndex);
-
-idSubset.forEach(id => {
-  this.getCultureArt(id);
-});
-
-this.count += this.twelve;
-
-        });
+        return {
+            Art: [],
+            Category: "",
+            count: 0,
+            twelve: 12
+        };
     },
-    getCultureArt(id) {
-      // Simple GET request using fetch
-      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`)
-        .then(response => response.json())
-        .then(data => {
-          this.Art.push(data);
-          console.log(data)
-        });
-    }
-  },
+    methods: {
+        showId(id) {
+            console.log(id);
+        },
+        setFrench() {
+            this.Category = "France";
+            console.log(this.Category);
+            this.Art = [];
+            this.twelve = 12;
+            this.count = 0;
+            this.getCultureID();
+        },
+        setChina() {
+            this.Category = "China";
+            console.log(this.Category);
+            this.Art = [];
+            this.twelve = 12;
+            this.count = 0;
+            this.getCultureID();
+        },
+        setRussia() {
+            this.Category = "Russia";
+            console.log(this.Category);
+            this.Art = [];
+            this.twelve = 12;
+            this.count = 0;
+            this.getCultureID();
+        },
+        setMexico() {
+            this.Category = "Mexico";
+            console.log(this.Category);
+            this.Art = [];
+            this.twelve = 12;
+            this.count = 0;
+            this.getCultureID();
+        },
+        setEurope() {
+            this.Category = "Europe";
+            console.log(this.Category);
+            this.Art = [];
+            this.twelve = 12;
+            this.count = 0;
+            this.getCultureID();
+        },
+        setIndia() {
+            this.Category = "India";
+            console.log(this.Category);
+            this.Art = [];
+            this.twelve = 12;
+            this.count = 0;
+            this.getCultureID();
+        },
+        setAfrica() {
+            this.Category = "Africa";
+            console.log(this.Category);
+            this.Art = [];
+            this.twelve = 12;
+            this.count = 0;
+            this.getCultureID();
+        },
+        getCultureID() {
+            // Simple GET request using fetch
+            fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?geoLocation=${this.Category}&q=flowers`)
+                .then(response => response.json())
+                .then(data => {
+                const objectID = data.objectIDs;
+                const startIndex = this.count;
+                const endIndex = this.count + this.twelve;
+                const idSubset = objectID.slice(startIndex, endIndex);
+                idSubset.forEach(id => {
+                    this.getCultureArt(id);
+                });
+                this.count += this.twelve;
+            });
+        },
+        getCultureArt(id) {
+            // Simple GET request using fetch
+            fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`)
+                .then(response => response.json())
+                .then(data => {
+                this.Art.push(data);
+                console.log(data);
+            });
+        }
+    },
+    components: { FlashCards }
 }
 </script>
