5b18e59c1418cdbf0fdd997ddf45add90c863dfe
Mount-Blanc <mountblanc100@gmail.com>
2023-04-01 13:26:34 -0500

    {changed vite config

--- MuseumArt/vite.config.js
+++ MuseumArt/vite.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
+  build: {
+    // disable build import analysis
+    analyze: false,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
