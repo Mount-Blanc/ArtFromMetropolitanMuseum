b9dc641f55b82122cac802665250e84cde31849e
Mount-Blanc <mountblanc100@gmail.com>
2023-04-01 13:46:12 -0500

    reinstalled plugin

--- MuseumArt/src/App.vue
+++ MuseumArt/src/App.vue
@@ -1,14 +1,25 @@
-<script setup>
-import Search from './components/Search.vue';
-import Works from './components/Works.vue';
-import Header from './components/Header.vue';
-</script>
+
 
 <template>
    <Header></Header>
    <Search></Search>
    <Works></Works>
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+import Header from './components/Header.vue';
+import Search from './components/Search.vue';
+import Works from './components/Works.vue';
+
+export default defineComponent({
+  components: {
+    Header,
+    Search,
+    Works,
+  },
+});
+</script>
 
 <style>
 body{
--- MuseumArt/vite.config.js
+++ MuseumArt/vite.config.js
@@ -1,10 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [vue()],
   build: {
     rollupOptions: {
       input: {
