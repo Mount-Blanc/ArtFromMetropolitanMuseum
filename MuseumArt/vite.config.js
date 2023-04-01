f5358e9a5e18a002854610f09abf83f76c54807f
Mount-Blanc <mountblanc100@gmail.com>
2023-04-01 13:49:38 -0500

    removed

--- MuseumArt/vite.config.js
+++ MuseumArt/vite.config.js
@@ -1,10 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [],
   build: {
     rollupOptions: {
       input: {
