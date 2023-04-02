c93b52d19f7ce83779501dbb73dd473294c2a36a
Mount-Blanc <mountblanc100@gmail.com>
2023-04-02 09:46:46 -0500

    added back vue/plugin in vite config

--- MuseumArt/vite.config.js
+++ MuseumArt/vite.config.js
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/ArtFromMetropolitanMuseum/",
-  plugins: [],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
