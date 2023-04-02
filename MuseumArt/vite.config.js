80ea09dd848df3dde7e13183b7b07714bc7ee48f
Mount-Blanc <mountblanc100@gmail.com>
2023-04-02 09:39:02 -0500

    removed additional settings from vite congf

--- MuseumArt/vite.config.js
+++ MuseumArt/vite.config.js
@@ -3,11 +3,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Art/",
+  base: "/ArtFromMetropolitanMuseum/",
   plugins: [],
-  optimizeDeps: {
-    include: [], // Set this to an empty array to disable import analysis
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
