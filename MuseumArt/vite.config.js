cdb2044f6ea35e156155a2b22c2cbbd6a5fce61c
Mount-Blanc <mountblanc100@gmail.com>
2023-04-02 09:32:24 -0500

    deleted root package.json

--- MuseumArt/vite.config.js
+++ MuseumArt/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Art/",
   plugins: [],
   optimizeDeps: {
     include: [], // Set this to an empty array to disable import analysis
